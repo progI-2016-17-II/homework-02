e1fae2ff34cb4cef8f57d948ed226e714f6a5f41
HeringAndras <hering.bandi@gmail.com>
2017-03-14 22:03:07 +0100

    2/1,2,3,4,5

---
+++ src/homework-02-functions.R
@@ -0,0 +1,29 @@
+#Házi feladat 2
+#Programozás I.
+#2016/17. II. félév
+#Hering András
+#2017.03.14
+
+#2/3
+
+get_gender <- function(name) {
+  if (is.na(comic_characters$gsm[comic_characters$name == name]) == T) {
+    paste(strsplit(comic_characters$sex[comic_characters$name == name]
+                   , " ", fixed = T)[[1]][1])
+  } else {
+    paste(strsplit(comic_characters$gsm[comic_characters$name == name]
+                   , " ", fixed = T)[[1]][1])
+  }
+}
+
+get_aligns <- function(align = "Bad Characters") {
+  if(is.na(align)){
+    paste(sort(comic_characters$name[is.na(comic_characters$align)],
+               decreasing = F))
+  } else {
+    paste(sort(comic_characters$name[comic_characters$align == align],
+               decreasing = F ))
+  }
+
+}
+
--- src/homework-02.R
+++ src/homework-02.R
@@ -1,1 +1,54 @@
-teszt
+#Házi feladat 2
+#Programozás I.
+#2016/17. II. félév
+#Hering András
+#2017.03.14
+
+#2.feladat
+#2/1
+if (!("fivethirtyeight" %in% installed.packages())) {
+  install.packages("fivethirtyeight")
+}
+library(fivethirtyeight)
+data("comic_characters")
+
+#2/2
+
+namesplit <- strsplit(comic_characters$name, " (", fixed = T)
+comic_characters$name <- sapply(namesplit,"[",1)
+# ismétlődések jönnek létre, ez eléggé megfúrja a get_gendert
+
+#2/3 funkció teszt
+
+# is.na(comic_characters$name["Mookie"])
+# get_gender("Mookie")
+# comic_characters$gsm[comic_characters$name == "Mookie"]
+# subset(comic_characters,comic_characters$name == "Raymonde Belmonde")
+# get_gender("Raymonde Belmonde")
+# subset(comic_characters,comic_characters$name == "Alma Chalmers")
+# get_gender("Alma Chalmers")
+
+#2/4
+
+get_gender("Katherine Pryde")
+get_gender("Loki Laufeyson")
+get_gender("Thor")
+
+#2/5 teszt
+
+# get_aligns()
+# get_aligns("Bad Characters")
+# get_aligns("Good Characters")
+# get_aligns("Neutral Characters")
+# get_aligns("Reformed Criminals")
+# get_aligns(NA)
+# 
+# sum(c(length(get_aligns()),length(get_aligns("Good Characters")),
+#       length(get_aligns("Neutral Characters")),length(get_aligns(NA)),
+#       length(get_aligns("Reformed Criminals"))))
+# summary(comic_characters$align)
+# jónak tűnik
+
+
+
+
