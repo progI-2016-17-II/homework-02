07a58df3fb1b3e81ef99bc245544c40c8fa836e0
Szodi0123 <adam.szilagyi.23@gmail.com>
2017-03-15 17:32:22 +0100

    Alakul, de még nem az igazi...

--- src/homework-02-functions.R
+++ src/homework-02-functions.R
@@ -4,3 +4,11 @@
 ##Szilágyi Ádám
 ##2017.03.15
                               ###FUNCTIONS###
+
+get_gender <- function(superhero) {
+  if (is.na(comic_characters$gsm[comic_characters$name == superhero]) == 1) {
+    print(substr(comic_characters$sex, 1, nchar(comic_characters$sex) - 11)
+          [[1]][1])
+} else {
+    print(comic_characters$gsm[comic_characters$name == superhero])[[1]][1]}
+  }
--- src/homework-02.R
+++ src/homework-02.R
@@ -4,4 +4,38 @@
 ##Programozás I.
 ##2016/17. II. félév
 ##Szilágyi Ádám
-##2017.03.15 
+##2017.03.15
+
+
+#II.1 feladat: Package installálás, behívás
+
+if (!("fivethirtyeight" %in% installed.packages())) {
+  install.packages("fivethirtyeight")
+}
+
+require(fivethirtyeight, quietly = TRUE)
+
+data(comic_characters)
+
+head(comic_characters)
+
+#II.2 feladat: Zárójelek levágása
+
+for (i in 1:nrow(comic_characters)) {
+  comic_characters$name[i] <-
+    unlist(strsplit(comic_characters$name[i],
+                    split = "(", fixed = TRUE))[1]
+  }
+
+comic_characters$name <-
+  substr(comic_characters$name, 1, nchar(comic_characters$name) - 1)
+
+#II.2 feladat
+
+source("src/homework-02-functions")
+
+
+get_gender("Thor")
+
+
+
