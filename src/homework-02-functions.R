02f0537658331fe8d9798b22b1136c7be4929d82
Szodi0123 <adam.szilagyi.23@gmail.com>
2017-03-15 11:16:04 +0100

    Elkészültek a fejlécek!

---
+++ src/homework-02-functions.R
@@ -0,0 +1,6 @@
+##Házi feladat 2
+##Programozás I.
+##2016/17. II. félév
+##Szilágyi Ádám
+##2017.03.15
+                              ###FUNCTIONS###
---
+++ src/homework-02.R
@@ -0,0 +1,7 @@
+# I. feladat
+
+##Házi feladat 2
+##Programozás I.
+##2016/17. II. félév
+##Szilágyi Ádám
+##2017.03.15 
